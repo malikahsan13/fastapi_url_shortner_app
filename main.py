21b56787fe9a74a36ab6043ce5c1df4062f1ad0b
Malik Ahsan <malikahsan13@gmail.com>
2026-04-05 23:52:09 +0500

    added sqlalchemy for db operations

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def main():
-    print("Hello from fastapi-url-shortner-app!")
-
-
-if __name__ == "__main__":
-    main()
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, HttpUrl
